3a44ce13d598a42860ce81f98ff7bac02be93518
Igor Tullio <igortullio@hotmail.com>
2019-10-31 17:39:25 -0300

    Objeto

---
+++ exercicios-js/objeto/objetosConstantes.js
@@ -0,0 +1,23 @@
+// pessoa -> 123 -> {...}
+const pessoa = {
+    nome: "Igor"
+}
+pessoa.nome = "Tullio"
+
+
+// pessoa -> 456 -> {...}
+// pessoa = {nome: "Ana"}
+
+Object.freeze(pessoa)
+pessoa.nome = "Maria"
+pessoa.end = "End"
+delete pessoa.nome
+console.log(pessoa.nome)
+console.log(pessoa)
+
+
+const pessoaConstante = Object.freeze({
+    nome: "João"
+})
+pessoaConstante.nome = "Maria"
+console.log(pessoaConstante)
