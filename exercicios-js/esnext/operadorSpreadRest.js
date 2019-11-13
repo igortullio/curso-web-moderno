5eb113dd7fe630ebd7e590e60ab22db46de7af78
Igor Tullio <igortullio@hotmail.com>
2019-11-13 17:53:42 -0300

    ESnext

---
+++ exercicios-js/esnext/operadorSpreadRest.js
@@ -0,0 +1,13 @@
+// Operador ... rest(juntar)/spread(espalhar)
+// usar rest como parâmetro de função
+
+// usar spread com objeto
+const funcionario = {nome: 'Maria', salario: 12348.99}
+const clone = {ativo: true, ...funcionario}
+console.log(clone)
+
+// usar spread com array
+const grupoA = ['João', 'Pedro', 'Glória']
+const grupoFinal = ['Maria', 'Rafaela', ...grupoA]
+console.log(grupoFinal)
+
